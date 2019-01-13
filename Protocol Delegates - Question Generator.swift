a14e5920a6d82b2b3364ba70b97ebf6c9887a3d3
Shivang Ranjan <shivang12134@gmail.com>
2019-01-13 11:47:54 +0530

    Power of Protocols

--- Protocol Delegates - Question Generator.swift
+++ Protocol Delegates - Question Generator.swift
@@ -26,7 +26,8 @@ enum QuestionType : String {
     case multipleChoice = "Who is the ugliest beatle : John ,Paul, george, or Ringo?"
     case shortAnswer = "What is the capital of UP?"
     case essay : "In 50 words, explain mlecular fusion"
-    
+ 
+    let types = [trueFalse, multipleChoice, shortAnswer, essay]
 }
 
 enum AnswerType : String {
@@ -34,16 +35,39 @@ enum AnswerType : String {
     case multipleChoice = "Srgt. Pepper"
     case shortAnswer = "Salem"
     case essay = "Molecular fusion happens when a daddy molecule and a mummy molecule love each other verryy much"
+    
+    static let types = [trueFalse, multipleChoice, shortAnswer, essay] //Enums may not contain stored property
+    
 }
+
 
 protocol  QuestionGenerator {
     func generateRandomFunction() -> Question
 }
+class Answer {}
+protocol AnswerGenerator : QuestionGenerator {
+    func generateRandomAnswer() -> Answer
+}
+
+class Jeopardy: AnswerGenerator {
+    generateRandomQuestion() -> Question
+}
 class Quiz: QuestionGenerator {
     func generateRandomQuestion() -> Question {
+        let randomNumeral = Int(arc4random_uniform(4))
+        let randomType = QuestionType.types[randomNumeral]
+        let randomQuery = randomType.rawValue
+        let randomAnswer = AnswerType.types[randomNumeral].rawValue
+        let randomQuestion = Question(type :randomType, query: randomQuery, answer: randomAnswer)
+        return randomQuestion
         
     }
 }
-//enumerations - How are enumerations, delegates and protocols used?
+//enumeations - How are enumerations, delegates and protocols used?
+
+let quiz = Quiz()
+let question =  quiz.generateRandomQuestion()
+
+print("QUESTION TYPE: \(question.type) \n QUERY: (question.query) \n ANSWER : (question.answer)")
 
 
