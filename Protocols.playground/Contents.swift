c738f6770f9e807352436bfaec8aa561d08f2c9d
Shivang Ranjan <shivang12134@gmail.com>
2019-01-13 09:58:48 +0530

    Protocol Delegates

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var str = "Hello, playground"
+//Protocol- blueprint can contain methods/properties. We can conform to a certain protocol from a class or from a structure or an enumeration.
+
+protocol Number {
+    
+    var floatValue: Float {
+        get
+    }
+}
+
+//conform to it just like subclassing/ a rectangle sub class
+extension Float: Number {
+    var floatValue : Float {
+        return self
+    }
+}
+
+
+
